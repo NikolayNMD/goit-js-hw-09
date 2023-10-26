381df14c33c504de92de9d0a3bbc122be599ed05
NikolayNMD <136238601+NikolayNMD@users.noreply.github.com>
2023-10-27 00:37:14 +0300

    third task done

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,2 +1,41 @@
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
 
+document.body.style.backgroundColor = '#beeeb9';
+
+const form = document.querySelector('.form');
+
+form.addEventListener('submit', onSubmitForm);
+
+function onSubmitForm(e) {
+  e.preventDefault();
+
+  let delay = Number(form.delay.value);
+
+  for (let i = 1; i <= form.amount.value; i += 1) {
+    createPromise(i, delay)
+      .then(({ position, delay }) => {
+        Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`);
+      })
+      .catch(({ position, delay }) => {
+        Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`);
+      });
+    delay += Number(form.step.value);
+  }
+}
+
+function createPromise(position, delay) {
+  const object = { position, delay };
+  const shouldResolve = Math.random() > 0.3;
+
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (shouldResolve) {
+        // Fulfill
+        resolve(object);
+      } else {
+        // Reject
+        reject(object);
+      }
+    }, delay);
+  });
+}
